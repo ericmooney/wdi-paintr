ced1bad093a7bf41c1252ed4ca0edf6cbed9d8d3
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-23 16:12:32 -0700

    Add ability to select background images

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -30,4 +30,32 @@ $(document).ready(function(){
   $('#add_color').on('click', add_color);
   $('#colors').on('click', '.box', set_color);
   $('.paint_box').on('mouseover', paint);
+
+
+  var add_photo = function(){
+    var box = $('<div>');
+    box.addClass('box');
+
+    var photo = $('#photo').val();
+    box.css('background-image', 'url('+photo+')');
+
+    $('#photos').prepend(box);
+    $('#photo').val('');
+
+  };
+
+  var set_image = function(){
+    var box = $(this);
+    var image = box.css('background-image');
+
+    $('#canvas').css('background-image', image);
+  };
+
+
+  $('#add_photo').on('click', add_photo);
+  $('#photos').on('click', '.box', set_image);
+
+
+
+
 });

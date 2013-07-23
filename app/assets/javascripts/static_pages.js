320a42ea00b431e09da66a1d2ff6fbf4f53b45f6
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-23 15:48:21 -0700

    Make paintable canvas

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -20,6 +20,14 @@ $(document).ready(function(){
     $('#selected_color').css('background-color', color);
   };
 
+  var paint = function(){
+    var paint_box = $(this);
+    var color = $('#selected_color').css('background-color');
+
+    paint_box.css('background-color', color);
+  };
+
   $('#add_color').on('click', add_color);
   $('#colors').on('click', '.box', set_color);
+  $('.paint_box').on('mouseover', paint);
 });
